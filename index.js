b58096543d07ef6474d009ae53c32718756f741a
VERA <zadorverochka@gmail.com>
2025-02-22 15:00:52 +0300

    add color and  name button

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const conteiner = document.getElementById('buttonsContainer');
+const container = document.getElementById('buttonsContainer');
 let activeButton = null;
-for ( let i = 0, i <= 9; i++) {
+const field = [];
+for (let j = 0; j <=3; j++) {
+    field.push()
+}
+for ( let i = 0; i <= 9; i++) {
     let button = document.createElement('button');
     button.textContent = `Поле ${i}`;
-    button.classList.add('btn', 'btn-danger');
+    button.classList.add('btn', 'button');
     button.addEventListener('click', () => {
         if ( activeButton === button) {
             activeButton.classList.remove('btn-success');
             activeButton.classList.add('btn-danger');
+            button.textContent = 'X';
             activeButton = null;
             return;
         }
-        if (activeButton) {
-            activeButton.classList.remove('btn-success');
-            activeButton.classList.add('btn-danger');
-        }
         button.classList.remove('btn-danger');
         button.classList.add('btn-success');
+        button.textContent = '0';
         activeButton = button;
     })
     container.appendChild(button); 
